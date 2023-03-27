ed94656ffba2a5a97c623185aff4d0aef5146a23
ETERNALPSY <tr.dev.manueldominguez@gmail.com>
2023-03-27 13:08:41 -0600

    feat: add conditional

--- src/index.js
+++ src/index.js
@@ -5,3 +5,15 @@ const postsDom = document.getElementById('posts-home');
 
 postsDom.appendChild( createCardPostHome(isLogged) );
 console.log(isLogged());
+
+
+//condicional para login y log out
+let logged = document.querySelectorAll(".login-card")
+let logOut = document.querySelectorAll(".logout-card")
+
+const token = () => {
+    let getToken = localStorage.getItem("token")
+    getToken ? logged.forEach(item => item.classList.add("d-none")) : logOut.forEach(item => item.classList.add("d-none"))
+}
+
+token()
